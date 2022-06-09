90654cb13315d3f4285b25356c78dbee111b84a1
programmer-jafir <jafiranjum2020@gmail.com>
2022-06-10 00:00:13 +0600

    contact addedr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Banner from './Component/Banner/Banner';
+import Contact from './Component/Contact/Contact';
 import Header from './Component/Head/Header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Banner></Banner>
+      <Contact></Contact>
     </div>
   );
 }
